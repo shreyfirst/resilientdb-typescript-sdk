e8b3a8fbeaec70d927f1ba227992d37f863a5a1e
samuelili <samuelli4521@gmail.com>
2023-11-27 17:18:06 -0800

    added several types

--- src/types.ts
+++ src/types.ts
@@ -12,7 +12,47 @@ export interface NetworkClient {
     )): Promise<TReturn>
 }
 
-// TODO: check
-export type Transaction = {
-  amount: number
+export type RetriveTransaction = {
+  id: string;
+  version: string;
+  amount: number; // integer
+  uri: string;
+  type: string;
+  publicKey: string;
+  operation: string;
+  metadata?: string | null;
+  asset: string;
+}
+
+export type CommitTransaction = {
+  id: string;
+}
+
+export type PrepareAsset = {
+  operation: string;
+  amount: number;
+  signerPublicKey: string;
+  signerPrivateKey: string;
+  recipientPublicKey: string;
+  asset: string;
+}
+
+export type UpdateAsset = {
+  id: string;
+  operation?: string | null;
+  amount?: number | null; // int
+  signerPublicKey: string;
+  signerPrivateKey: string;
+  recipientPublicKey?: string | null;
+  asset?: string | null;
+}
+
+export type FilterKeys = {
+  ownerPublicKey?: string | null;
+  recipientPublicKey?: string | null;
+}
+
+export type Keys = {
+  publicKey: string
+  privateKey: string
 }
