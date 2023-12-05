24d2257e602c5631a6f8808053bfc37b17e629e3
samuelili <samuelli4521@gmail.com>
2023-12-04 23:56:08 -0800

    fixed package.json and module declarations

--- tests/FetchClient.test.ts
+++ tests/FetchClient.test.ts
@@ -1,19 +1,19 @@
-import NodeFetchClient from "../src/NodeFetchClient.ts";
+import FetchClient from "../src/FetchClient.ts";
 import ResilientDB from "../src/ResilientDB.ts";
 
 test('NodeFetchClient initializes', () => {
-  const client = new NodeFetchClient();
+  const client = new FetchClient();
 });
 
 test('ResilientDB with NodeFetchClient', async () => {
-  const client = new ResilientDB("http://localhost:8000", new NodeFetchClient());
+  const client = new ResilientDB("http://localhost:8000", new FetchClient());
 
   const transactions = await client.getAllTransactions(); // make a request and see if it works
   expect(transactions !== undefined);
 });
 
 test('ResilientDB with NodeFetchClient and mutation', async () => {
-  const client = new ResilientDB("http://localhost:8000", new NodeFetchClient());
+  const client = new ResilientDB("http://localhost:8000", new FetchClient());
 
   const senderPair = ResilientDB.generateKeys();
   const receiverPair = ResilientDB.generateKeys();
--- tests/ResilientDB.test.ts
+++ tests/ResilientDB.test.ts
@@ -1,12 +1,12 @@
-import NodeFetchClient from '../src/NodeFetchClient.ts';
+import FetchClient from '../src/FetchClient.ts';
 import ResilientDB from '../src/ResilientDB.ts';
 
 test('ResilientDB instance initializes', () => {
-  const client = new ResilientDB("http://localhost:8000", new NodeFetchClient());
+  const client = new ResilientDB("http://localhost:8000", new FetchClient());
 });
 
 test('postTransaction: 1 transaction', async () => {
-  const client = new ResilientDB("http://localhost:8000", new NodeFetchClient());
+  const client = new ResilientDB("http://localhost:8000", new FetchClient());
 
   const senderPair = ResilientDB.generateKeys();
   const receiverPair = ResilientDB.generateKeys();
@@ -22,7 +22,7 @@ test('postTransaction: 1 transaction', async () => {
 });
 
 test('getAllTransactions', async () => {
-  const client = new ResilientDB("http://localhost:8000", new NodeFetchClient());
+  const client = new ResilientDB("http://localhost:8000", new FetchClient());
 
   const transactions = await client.getAllTransactions();
   expect(transactions !== undefined)
